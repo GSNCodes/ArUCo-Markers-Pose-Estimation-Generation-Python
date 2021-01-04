c43dcbcf7b46383c95bc6256ccb3841cc32ce406
GSNCodes <gsnspidey@gmail.com>
2021-01-04 22:38:27 +0530

    Pose estimation and Calibration code

---
+++ calibration.py
@@ -0,0 +1,82 @@
+'''
+Sample Usage:-
+python calibration.py --dir calibration_checkerboard/ --square_size 0.024
+'''
+
+import numpy as np
+import cv2
+import os
+import argparse
+
+
+def calibrate(dirpath, square_size, width=9, height=6, visualize=False):
+    """ Apply camera calibration operation for images in the given directory path. """
+
+    # termination criteria
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(8,6,0)
+    objp = np.zeros((height*width, 3), np.float32)
+    objp[:, :2] = np.mgrid[0:width, 0:height].T.reshape(-1, 2)
+
+    objp = objp * square_size
+
+    # Arrays to store object points and image points from all the images.
+    objpoints = []  # 3d point in real world space
+    imgpoints = []  # 2d points in image plane.
+
+    images = os.listdir(dirpath)
+
+    for fname in images:
+        img = cv2.imread(os.path.join(dirpath, fname))
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # Find the chess board corners
+        ret, corners = cv2.findChessboardCorners(gray, (width, height), None)
+
+        # If found, add object points, image points (after refining them)
+        if ret:
+            objpoints.append(objp)
+
+            corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+            imgpoints.append(corners2)
+
+            # Draw and display the corners
+            img = cv2.drawChessboardCorners(img, (width, height), corners2, ret)
+
+        if visualize:
+            cv2.imshow('img',img)
+            cv2.waitKey(0)
+
+
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+    return [ret, mtx, dist, rvecs, tvecs]
+
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-d", "--dir", required=True, help="Path to folder containing checkerboard images for calibration")
+    ap.add_argument("-w", "--width", type=int, help="Width of checkerboard (default=9)")
+    ap.add_argument("-t", "--height", type=int, help="Height of checkerboard (default=6)")
+    ap.add_argument("-s", "--square_size", type=float, default=1, help="Length of one edge (in metres)")
+    ap.add_argument("-v", "--visualize", type=str, default="False", help="To visualize each checkerboard image")
+    args = vars(ap.parse_args())
+    
+    dirpath = args['dir']
+    # 2.4 cm == 0.024 m
+    # square_size = 0.024
+    square_size = args['square_size']
+
+    if args["visualize"].lower() == "true":
+        visualize = True
+    else:
+        visualize = False
+
+    ret, mtx, dist, rvecs, tvecs = calibrate(dirpath, square_size, visualize=visualize)
+
+    print(mtx)
+    print(dist)
+
+    np.save("calibration_matrix", mtx)
+    np.save("distortion_coefficients", dist)
---
+++ pose_estimation.py
@@ -0,0 +1,79 @@
+'''
+Sample Usage:-
+python pose_estimation.py --K_Matrix calibration_matrix.npy --D_Coeff distortion_coefficients.npy
+'''
+
+
+import numpy as np
+import cv2
+import cv2.aruco as aruco
+import argparse
+import time
+
+
+def pose_esitmation(frame, matrix_coefficients, distortion_coefficients):
+
+    '''
+    frame - Frame from the video stream
+    matrix_coefficients - Intrinsic matrix of the calibrated camera
+    distortion_coefficients - Distortion coefficients associated with your camera
+
+    return:-
+    frame - The frame with the axis drawn on it
+    '''
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    aruco_dict = aruco.Dictionary_get(aruco.DICT_5X5_250)
+    parameters = aruco.DetectorParameters_create()
+
+
+    corners, ids, rejected_img_points = aruco.detectMarkers(gray, aruco_dict,parameters=parameters,
+        cameraMatrix=matrix_coefficients,
+        distCoeff=distortion_coefficients)
+
+        # If markers are detected
+    if len(corners) > 0:
+        for i in range(0, len(ids)):
+            # Estimate pose of each marker and return the values rvec and tvec---(different from those of camera coefficients)
+            rvec, tvec, markerPoints = aruco.estimatePoseSingleMarkers(corners[i], 0.02, matrix_coefficients,
+                                                                       distortion_coefficients)
+            # Draw a square around the markers
+            aruco.drawDetectedMarkers(frame, corners) 
+
+            # Draw Axis
+            aruco.drawAxis(frame, matrix_coefficients, distortion_coefficients, rvec, tvec, 0.01)  
+
+    return frame
+
+if __name__ == '__main__':
+
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-k", "--K_Matrix", required=True, help="Path to calibration matrix (numpy file)")
+    ap.add_argument("-d", "--D_Coeff", required=True, help="Path to distortion coefficients (numpy file)")
+    args = vars(ap.parse_args())
+
+    calibration_matrix_path = args["K_Matrix"]
+    distortion_coefficients_path = args["D_Coeff"]
+    
+    k = np.load(calibration_matrix_path)
+    d = np.load(distortion_coefficients_path)
+
+    video = cv2.VideoCapture(0)
+    time.sleep(2.0)
+
+    while True:
+        ret, frame = video.read()
+
+        if not ret:
+            break
+        
+        output = pose_esitmation(frame, k, d)
+
+        cv2.imshow('Estimated Pose', output)
+
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('q'):
+            break
+
+    video.release()
+    cv2.destroyAllWindows()
